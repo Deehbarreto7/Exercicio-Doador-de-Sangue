e6152994d3f0ceb3951f98dcd0c5eba158fc8b67
Débora <deehbarreto7@gmail.com>
2026-05-08 21:22:57 -0300

    doador de sangue

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Qual é a sua idade?");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67)
+{ 
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você pode ser doador de sangue.");
+}
+else 
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você não pode ser doador de sangue.");
+}
+
+Console.ResetColor();
+Console.WriteLine("Obrigado!");
